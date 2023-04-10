f3304d7991b8e47bd8a60d8ee3ce07a79aef3b3e
Valentin Eduardo Gonzalez Morales <valentin.eduardo.g94@gmail.com>
2023-04-10 17:53:00 -0500

    creating button

--- packages/ui/Button/index.tsx
+++ packages/ui/Button/index.tsx
@@ -1,6 +1,28 @@
 import * as React from "react";
-import "./button.styles.css";
+import "./button.styles.module.css";
+import clsx from "clsx";
 
-export const Button = () => {
-  return <button className="button">Hi</button>;
+export interface IButtonProps {
+  children: React.ReactNode;
+  text?: string;
+  variant: "primary" | "secondary";
+  size: "small" | "large";
+  icon?: string;
+}
+
+export const Button: React.FC<IButtonProps> = (props) => {
+  const { children, size, variant, icon, text, ...rest } = props;
+  return (
+    <button
+      className={clsx(size === "large" ? "large" : "small", variant)}
+      {...rest}
+    >
+      {icon && (
+        <picture>
+          <img src={icon} alt="" />
+        </picture>
+      )}
+      <span>{text ?? children}</span>
+    </button>
+  );
 };
